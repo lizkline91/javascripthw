90746720f31261dcb22964bc0db7115ebb7897be
lizkline91 <lizkline91@gmail.com>
2014-07-31 08:55:07 -0400

    redo

---
+++ datatypes.js
@@ -0,0 +1,80 @@
+//Boolean//
+
+  //1//
+    "My name is Liz".length > 5;
+
+  //2//
+    5 + 4 >= 8;
+
+  //3//
+    "Will this work?".length > 20;
+
+//Integer//
+
+  //1//
+    5
+
+  //2//
+    var b = 3
+
+  //3//
+
+//string//
+
+  //1//
+    "My Name is Liz"
+
+  //2//
+    "1, 2, 3, 4"
+
+  //3//
+    1 + "1"
+
+//Object//
+  //1//
+    var myObj = {
+      name:"Liz",
+      age:"23",
+      hobby:"cooking",
+    };
+
+  //2//
+    var myObj = {
+      number:"555-4342",
+      address:"11024 lightway",
+      state:"California",
+    }
+
+  //3//
+    var myDog = {
+      age: "3",
+      color: "tan",
+      eyes: "honey",
+    }
+
+
+//Array//
+  //1//
+    var colors = [];
+    colors.push("purple", "green", "red", "orange");
+
+  //2//
+    var numbers = [];
+    numbers.push("1","2","3","4");
+    numbers.reverse();
+
+  //3//
+    var numbers = [];
+    numbers.push("1","2","3","4","5");
+    numbers.pop();
+
+
+//Function//
+  //1//
+    var liz = function (){
+      console.log("liz");
+    };
+    liz();
+  //2//
+    var
+  //3//
